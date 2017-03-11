f49032e4254e6a080bf014085b3a699ff740e1e1
Joshua Castor <jcasto3@uic.edu>
2017-03-10 21:50:23 -0600

    Declared function for adding modules

--- server-nodejs/server.js
+++ server-nodejs/server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+// Create application/x-www-form-urlencoded parser
+// Used in functions related to POST
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.get('/', function(request,response){
     response.end("Welcome to the homepage.");
 });
@@ -25,6 +29,20 @@ app.get('/listModules/:type', function(request,response){
     console.log("Received request for listModules");
     response.end("this is the list modules api call for type " + request.params.type + " in the server");
 });
+
+app.post('/addModule', urlencodedParser, function(request,response){
+    var data = JSON.parse(request.body.data);
+    var type = request.body.type.toLowerCase();
+    if(type.equals("user")){
+        addUser(data);
+    }else if(type.equals("module")){
+        addModule(data);
+    }else{
+        console.log("addModule: Invalid data type received");
+        console.log(data);
+    }
+    response.end();
+})
 
 var server = app.listen(80, function(){
     var host = server.address().address;
